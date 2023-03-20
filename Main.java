ed5da8f44f263b74ac0cda1ac6204650840085fb
Marloto <marloto@codestack.de>
2023-03-20 14:28:22 +0100

    16.03.2023

--- Main.java
+++ Main.java
@@ -1,5 +1,29 @@
 public class Main {
+  // eine Funktion könnte man aktuell wie folgt definieren
+  static int ggt(int zahl1, int zahl2) {
+    // ...
+    return 123;
+  }
+
+  // main muss genau so definiert werden wie folgend
   public static void main(String[] args) {
-        System.out.println("Hello world!");
+    int z = ggt(4, 8);
+
+    String someStr = "";
+    String someStr2 = "Hello"; // Debugger nutzen und prüfen, was in someStr gespeichert wird
+    int x = 42;
+    System.out.println(someStr2 + " world!");
+    System.out.println(x);
+    int y = x + 1;
+    System.out.println("Ergebnis: ");
+    System.out.println(y);
+    System.out.printf("");
+
+    // Datentyp ist java.util.Scanner
+    // Name der Variable ist scanner
+    // Wert ist new java.util.Scanner(System.in)
+    java.util.Scanner scanner = new java.util.Scanner(System.in);
+    int x2 = scanner.nextInt();
+    System.out.println("Eingabe war: " + x2);
   }
 }
---
+++ Something.java
@@ -0,0 +1,28 @@
+public class Something {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int resGgT = ggT(a, b);
+        int resKgV = kgV(a, b);
+        System.out.print("Ergebnis ggT: ");
+        System.out.println(resGgT);
+        System.out.print("Ergebnis kgV: ");
+        System.out.println(resKgV);
+    }
+    public static int kgV (int a, int b){
+        return (a*b) / ggT(a,b);
+    }
+    public static int ggT(int zahl1, int zahl2) {
+        int rest, ggt;
+        rest=0;
+        do {
+            ggt= rest;
+            rest = zahl1 % zahl2;
+            zahl1 = zahl2;
+            zahl2 =rest;
+        }while(rest !=0);
+        return ggt;
+    }
+}
