786f88caa37938349aab0e74e2144609254a7806
Marloto <marloto@codestack.de>
2023-03-23 14:59:15 +0100

    Updates 23.03.2023

---
+++ src/Farbe.java
@@ -0,0 +1,41 @@
+
+public class Farbe {
+	byte rot;
+	byte gruen;
+	byte blau;
+	
+	// ... methods ...
+	
+	Farbe() {
+		this((byte) -1);
+	}
+	
+	Farbe(byte grau) {
+		this(grau, grau, grau);
+	}
+	
+	Farbe(byte r, byte g, byte b) {
+		// pruefen ob sie in einem richtigen bereich sind
+		//if(-128 >= rot && rot < 127) {
+		//}
+		rot = r;
+		gruen = g;
+		blau = b;
+	}
+	
+	Farbe(byte r, byte g, double b) {
+		// pruefen ob sie in einem richtigen bereich sind
+		//if(-128 >= rot && rot < 127) {
+		//}
+		rot = r;
+		gruen = g;
+		blau = (byte) b;
+	}
+	
+	public static void main(String[] args) {
+		int b = 1;
+		
+		Farbe f = new Farbe((byte) 122, (byte) 0, b);
+		
+	}
+}
---
+++ src/ThiNutzerKonto.java
@@ -0,0 +1,11 @@
+
+public class ThiNutzerKonto {
+	String nameDesStudierenden;
+	int alterDesHaustieres;
+	int anzahlDerRaeume;
+	
+	void berechneKontoStand() {}
+	void ausgebenAufKonsole() {}
+	void gibId() {}
+	void erzeugeHttpVerbindung() {}
+}
