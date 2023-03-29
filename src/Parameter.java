5768bac4cf1da28a8ae12eafcd89d69571bd04b9
Marloto <marloto@codestack.de>
2023-03-29 18:12:33 +0200

    added small changes

---
+++ src/Parameter.java
@@ -0,0 +1,28 @@
+
+class Morething {
+	int a;
+}
+
+
+public class Parameter {
+	
+	void doSomething(int x) {
+		x ++;
+	}
+	void doSomething(Morething obj) {
+		obj.a ++;
+	}
+	
+	public static void main(String[] args) {
+		Parameter p = new Parameter();
+		Morething m = new Morething();
+		
+		m.a = 12;
+		p.doSomething(m);
+		System.out.println(m.a);
+		
+		int y = 12;
+		p.doSomething(y);
+		System.out.println(y);
+	}
+}
